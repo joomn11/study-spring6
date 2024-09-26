9394be51bab3a494f90702a22e1d44c7ed7d61ef
joomn11 <joomn11@gmail.com>
2024-09-26 18:48:58 +0900

    상속을 통한 확장

--- src/main/java/tobyspring/myboot/PaymentService.java
+++ src/main/java/tobyspring/myboot/PaymentService.java
@@ -1,47 +1,21 @@
 package tobyspring.myboot;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
 
-public class PaymentService {
+public abstract class PaymentService {
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        // get currency
         BigDecimal exRate = getExRate(currency);
-
-        // calculate convertedAmount
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-
-        // make valid period
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30L);
 
         return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
     }
 
-    private BigDecimal getExRate(String currency) throws IOException {
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
+    abstract BigDecimal getExRate(String currency) throws IOException;
 
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData data = mapper.readValue(response, ExRateData.class);
-        return data.rates().get("KRW");
-    }
-
-    public static void main(String[] args) throws IOException {
-        PaymentService service = new PaymentService();
-        Payment payment = service.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment);
-    }
 }
 
 
