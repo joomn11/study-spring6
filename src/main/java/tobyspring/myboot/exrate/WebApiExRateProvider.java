88e063889d3191cac96cb2fd8f7187079e6fbf51
joomn11 <joomn11@gmail.com>
2024-10-23 20:34:02 +0900

    템플릿, 콜백 분리

--- src/main/java/tobyspring/myboot/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/myboot/exrate/WebApiExRateProvider.java
@@ -1,60 +1,19 @@
 package tobyspring.myboot.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.stream.Collectors;
+import tobyspring.myboot.api.ApiTemplate;
+import tobyspring.myboot.api.ErApiExRateExtractor;
+import tobyspring.myboot.api.SimpleApiExecutor;
 import tobyspring.myboot.payment.ExRateProvider;
 
 //@Component
 public class WebApiExRateProvider implements ExRateProvider {
 
+    ApiTemplate apiTemplate = new ApiTemplate();
+
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
-        return runApiForExRate(url);
-    }
-
-    private BigDecimal runApiForExRate(String url) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = executeApi(uri);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            return extractExRate(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private BigDecimal extractExRate(String response) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData data = mapper.readValue(response, ExRateData.class);
-        return data.rates().get("KRW");
-    }
-
-    private String executeApi(URI uri) throws IOException {
-        String response;
-        HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-            response = br.lines().collect(Collectors.joining());
-        }
-        return response;
+        return apiTemplate.getExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
     }
 }
