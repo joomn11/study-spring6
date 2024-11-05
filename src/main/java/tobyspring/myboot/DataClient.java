c911e4b84f40615a71ee05e334a0bd9e8eea5f9d
joomn11 <joomn11@gmail.com>
2024-11-05 18:50:12 +0900

    DataAccessException 예외 추상화

--- src/main/java/tobyspring/myboot/DataClient.java
+++ src/main/java/tobyspring/myboot/DataClient.java
@@ -3,6 +3,9 @@ package tobyspring.myboot;
 import java.math.BigDecimal;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import tobyspring.myboot.data.OrderRepository;
 import tobyspring.myboot.order.Order;
 
@@ -13,14 +16,24 @@ public class DataClient {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
 //        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
         OrderRepository repository = beanFactory.getBean(OrderRepository.class);
+        JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
-        Order order = new Order("100", BigDecimal.TEN);
-        repository.save(order);
+        try {
+            new TransactionTemplate(transactionManager).execute(status -> {
+                Order order = new Order("100", BigDecimal.TEN);
+                repository.save(order);
 
-        System.out.println(order);
+                System.out.println(order);
 
-        Order order2 = new Order("100", BigDecimal.ONE);
-        repository.save(order2);
+                Order order2 = new Order("100", BigDecimal.ONE);
+                repository.save(order2);
+                return null;
+            });
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            System.out.println("restore process..");
+        }
+
 
     }
 }
